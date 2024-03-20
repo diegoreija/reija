f3a4d5a54c78001451070a615be50ec321a3024a
Reija21 <>
2024-03-21 00:14:50 +0100

    Updates

--- pages/certificates/script.js
+++ pages/certificates/script.js
@@ -51,20 +51,20 @@ dropdowns.forEach(function(dropdown) {
     */
 
 
-    let listElements = document.querySelectorAll('.list_button_click');
+let listElements = document.querySelectorAll('.list_button_click');
 
-    listElements.forEach(listElement => {
-      listElement.addEventListener('click',()=>{
+listElements.forEach(listElement => {
+   listElement.addEventListener('click',()=>{
+      listElement.classList.toggle('arrow');
 
-        listElement.classList.toggle('arrow');
+      let height = 0;
+      let menu = listElement.nextElementSibling;
+      if(menu.clientHeight == "0"){
+        height = menu.scrollHeight;
+      }
 
-        let height = 0;
-        let menu = listElement.nextElementSibling;
-        if(menu.clientHeight == "0"){
-          height = menu.scrollHeight;
-        }
+      menu.style.height = height + "px";
 
-        menu.style.height = height + "px";
+   })
+});
 
-      })
-    });

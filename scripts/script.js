bda43e86d7af947d79e702ed0a3eb85919db37ed
diegoreija <diegoreija21@gmail.com>
2025-04-06 09:36:13 +0200

    miau

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const menuButton = document.getElementById("menuButton");
 const sidebar = document.getElementById("sidebar");
 const closeButton = document.getElementById("closeButton");
@@ -83,48 +82,3 @@ document.getElementById('contactForm').addEventListener('submit', function(event
         });
 });
 
-=======
-var nextBtn = document.querySelector('.next'),
-    prevBtn = document.querySelector('.prev'),
-    carousel = document.querySelector('.carousel'),
-    list = document.querySelector('.list'), 
-    item = document.querySelectorAll('.item'),
-    runningTime = document.querySelector('.carousel .timeRunning') 
-
-
-nextBtn.onclick = function(){
-    showSlider('next')
-}
-
-prevBtn.onclick = function(){
-    showSlider('prev')
-}
-
-
-function resetTimeAnimation() {
-    runningTime.style.animation = 'none'
-    runningTime.offsetHeight /* trigger reflow */
-    runningTime.style.animation = null 
-    runningTime.style.animation = 'runningTime 7s linear 1 forwards'
-}
-
-
-function showSlider(type) {
-    let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
-    if(type === 'next'){
-        list.appendChild(sliderItemsDom[0])
-        carousel.classList.add('next')
-    } else{
-        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
-        carousel.classList.add('prev')
-    }
-
-    clearTimeout(runTimeOut)
-
-    runTimeOut = setTimeout( () => {
-        carousel.classList.remove('next')
-        carousel.classList.remove('prev')
-    }, timeRunning)
-
-}
->>>>>>> 0ec6c47d7f24ced086278d288d812a94c11f1ab2
